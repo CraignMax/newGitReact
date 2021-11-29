9ee1b6f7a0a192b8095ef47e7d6a050a4c8c87ef
Glen Adamson <glen.adamson@terraquest.co.uk>
2021-11-29 10:01:03 +0000

    made some updates - the code you need to write for the story your working on is inside app.js - i have left comments in there

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import './App.css';
+
+// this is the entry point for you app and is what your seeing on screen, i put the index.html back into the public folder
+// and reset its contents.
+
+// the below is where you need to add your code.
+const App = () => (
+    <div>
+        {/*you will need to add your logo here*/}
+        {/*<----logo html element----->*/}
+        you will need to add your 2 input elements - 1 for user name and 1 for password
+        {/*<--------input elements - example <input placeholder="i am glen" /> - ---------->*/}
+    </div>
+);
+
+export default App;
 
